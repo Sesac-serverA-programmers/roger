e71d3ea882c5c813a73dcbbf513b3c1c360b7157
yawnsoo <0607yeonsoo@gmail.com>
2022-12-10 23:58:10 +0900

    프로그래머스 옹알이

---
+++ ReadyCodingTest/src/main/java/programmers/ongari/Main.java
@@ -0,0 +1,35 @@
+package programmers.ongari;
+// https://school.programmers.co.kr/learn/courses/30/lessons/120956
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+
+    List<String> list = new ArrayList<>(Arrays.asList("aya", "ye", "woo", "ma"));
+
+
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for (String s : babbling) {
+            String tmp = "";
+            for (String l : list) {
+                if(s.contains(l)){
+                    tmp += l;
+                }
+            }
+            if (tmp.length() == s.length()) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int sol = new Main().solution(new String[]{"ayaye", "uuuma", "ye", "yemawoo", "ayaa","mawyeoo","yemawooye"});
+        System.out.println(sol);
+    }
+}
