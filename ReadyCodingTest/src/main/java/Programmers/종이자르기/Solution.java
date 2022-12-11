9844bf447d8068ecf45addc1a364d89e77529242
yawnsoo <0607yeonsoo@gmail.com>
2022-12-11 18:34:20 +0900

    프로그래머스 종이자르기

---
+++ ReadyCodingTest/src/main/java/Programmers/종이자르기/Solution.java
@@ -0,0 +1,18 @@
+package Programmers.종이자르기;
+
+class Solution {
+    public int solution(int M, int N) {
+        int answer = 0;
+        return M*(N-1)+(M-1);
+    }
+
+    public static void main(String[] args) {
+        int sol = new Solution().solution(2,2);
+        int sol2 = new Solution().solution(2,5);
+        int sol3 = new Solution().solution(1,1);
+
+        System.out.println(sol);
+        System.out.println(sol2);
+        System.out.println(sol3);
+    }
+}
